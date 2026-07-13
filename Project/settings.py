bd9c4ca2aeb6559a4017007f76a8fea2309b5184
sivaprakashdev <sivaprakash@inessconsulting.com>
2026-07-13 12:47:00 +0530

    fix-deploy

--- Project/settings.py
+++ Project/settings.py
@@ -114,26 +114,26 @@ SESSION_ENGINE = "django.contrib.sessions.backends.cached_db"
 #     }
 # }
 
-DATABASES = {
-    'default':{
-        'ENGINE':'django.db.backends.postgresql',
-        'NAME':os.getenv('DB_NAME'),
-        'USER':os.getenv('DB_USER'),
-        'PASSWORD':os.getenv('DB_PASSWORD'),
-        'HOST':os.getenv('DB_HOST'),
-        'PORT':5432,
-    }
-}
-
-# import dj_database_url
-
 # DATABASES = {
-#     "default": dj_database_url.config(
-#         default=os.environ.get("DATABASE_URL"),
-#         conn_max_age=600,
-#         ssl_require=False,
-#     )
+#     'default':{
+#         'ENGINE':'django.db.backends.postgresql',
+#         'NAME':os.getenv('DB_NAME'),
+#         'USER':os.getenv('DB_USER'),
+#         'PASSWORD':os.getenv('DB_PASSWORD'),
+#         'HOST':os.getenv('DB_HOST'),
+#         'PORT':5432,
+#     }
 # }
+
+import dj_database_url
+
+DATABASES = {
+    "default": dj_database_url.config(
+        default=os.environ.get("DATABASE_URL"),
+        conn_max_age=600,
+        ssl_require=False,
+    )
+}
 
 
 # default="redis://localhost:6379/0"
